30e0e290d4d11a30cfeed282dbf3676fbab3b472
Darrin Kachadorian <dkachado@depaul.edu>
2022-10-12 12:51:48 -0400

    added the delete reducer component

--- src/Reducers.js
+++ src/Reducers.js
@@ -23,28 +23,13 @@ function toDoReducer(state, action) {
         dateCompleted: action.dateCompleted,
       };
       return [newToDo, ...state];
-    default:
-      return state;
-  }
-}
 
-// add new reducer code here -- such as TOGGLE_TODO --  When user clicks the checkbox
-function toggleToDoReducer(state, action) {
-  switch (action.type) {
-    case "TOGGLE":
-      return state.filter((state) => state.id !== action.id);
-    default:
-      throw new Error();
-  }
-}
-
-// add new reducer code here --- such as DELETE_TODO -- iterates over all the ToDos held in state to identify a todo for removal from state
-function deleteToDoReducer(state, action) {
-  switch (action.type) {
+    // add the delete todo component
     case "DELETE_TODO":
       return state.filter((state) => state.id !== action.id);
+
     default:
-      throw new Error();
+      return state;
   }
 }
 
@@ -52,7 +37,5 @@ export default function appReducer(state, action) {
   return {
     user: userReducer(state.user, action),
     toDos: toDoReducer(state.toDos, action),
-    //toggle: toggleToDoReducer(state.toggle, action),
-    // delete: deleteToDoReducer(state.delete, action),
   };
 }
